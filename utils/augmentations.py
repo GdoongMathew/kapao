70f04930d9930bf561242d33b687772ccccbe9c6
GdoongMathew <dungdudu426@gmail.com>
2022-03-10 00:11:22 +0800

    fix label shape mismatch after albumentation augmentation.

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -37,9 +37,10 @@ class Albumentations:
     def __call__(self, im, labels, p=1.0):
         if self.transform and random.random() < p:
             new = self.transform(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0])  # transformed
-            im, labels = new['image'], np.array([[c, *b] for c, b in zip(new['class_labels'], new['bboxes'])])
+            im = new['image']
+            if len(labels):
+                labels = np.array([[c, *b] for c, b in zip(new['class_labels'], new['bboxes'])])
         return im, labels
-
 
 def augment_hsv(im, hgain=0.5, sgain=0.5, vgain=0.5):
     # HSV color-space augmentation
